d6dba8f60d76724038b8b9c3701942f4a05fbeb8
Maly Phongsavanh <phongsavanh619@icloud.com>
2018-08-10 16:26:26 -0700

    show images to if else once form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
   alert("omg!")
   $(".form-group").submit(function(event) {
-    alert("fuck")
+    alert("this part works")
     var que1 = $("#question1").val();
     var que2 = $("#question2").val();
     var que3 = $("#question3").val();
     var que4 = $("#question4").val();
     var que5 = $("#question5").val();
+// image id tags
 
     if (que1 === '2') {
         alert("This survey is not meant for you!")
         }
       else if (que2 === '2' && que3 === '2') {
-        // $(".programLang").show_image('photo/php.png')
+        $(".php").show();
         alert("this is php!")
         }
       else if (que3 === '3' && que4 === '2') {
-        // $(".programLang").show_image('photo/csharp.png')
+        $(".sharp").show();
         alert("this is sharp##")
         }
       else if (que2 === '3' && que3 === '1') {
-          // $(".programLang").show_image('photo/JS.png')
+          $(".java").show();
         alert("this is javaaaa")
         }
     event.preventDefault();
